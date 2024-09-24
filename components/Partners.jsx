a80fbb11b1c999166dd3a93efb9d2925b622f883
uniquetheo <unique.theo98@gmail.com>
2024-09-24 16:40:15 +0000

    Feat: Partners

--- components/Partners.jsx
+++ components/Partners.jsx
@@ -1,16 +1,26 @@
 import { businessPartners } from "@/data/dummy";
 import Image from "next/image";
 import React from "react";
+import airbnb from "../public/images/airbnb.svg";
+import flyEmirates from "../public/images/flyEmirates.svg";
+import trivago from "../public/images/trivago.svg";
+import turkishAirlines from "../public/images/turkish-airlines.svg";
+import swiss from "../public/images/swiss.svg";
 
+const images = [flyEmirates, trivago, airbnb, turkishAirlines, swiss];
 const PartnersSection = () => {
   return (
-    <div>
-      {businessPartners.map((partner, idx) => (
-        <div
-          key={idx}
-          className="flex relative justify-center items-center h-20"
-        >
-          <Image src={partner.image} alt={partner.name} fill className="" />
+    <div className="flex w-full justify-between max-w-7xl py-8 mx-auto ">
+      {images.map((partner, idx) => (
+        <div key={idx} className="flex justify-center items-center h-20">
+          <Image
+            src={images[idx]}
+            alt={partner}
+            // fill
+            width={150}
+            height={150}
+            className="z-40"
+          />
         </div>
       ))}
     </div>
