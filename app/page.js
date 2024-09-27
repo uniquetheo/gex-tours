d088e6064ba25153dd6e78c93cb62e2edec72e24
uniquetheo <unique.theo98@gmail.com>
2024-09-27 11:13:06 +0000

    added why us section, included in package section

--- app/page.js
+++ app/page.js
@@ -2,10 +2,12 @@ import ContactUs from "@/components/ContactUs";
 import FaqSection from "@/components/FaqSection";
 import FeaturedTrips from "@/components/FeaturedTripsSection";
 import HeroSection from "@/components/HeroSection";
+import IncludedInPackage from "@/components/IncludedInPackage";
 import Newsletter from "@/components/NewsLetter";
 import PackagesSection from "@/components/PackagesSection";
 import PartnersSection from "@/components/Partners";
 import ServicesSection from "@/components/ServicesSection";
+import WhyUs from "@/components/WhyUsSection";
 
 export default function Home() {
   return (
@@ -14,8 +16,9 @@ export default function Home() {
       <PartnersSection />
       <div className="border-b" />
       <FeaturedTrips />
-      {/* <ServicesSection /> */}
+      <WhyUs />
       <PackagesSection />
+      <IncludedInPackage />
       <div className="container mx-auto py-10 grid md:grid-cols-[500px_1fr]">
         <FaqSection />
         <Newsletter />
---
+++ components/IncludedInPackage.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const IncludedInPackage = () => {
+  return (
+    <div className="container mx-auto px-4 md:px-8 py-10">
+      <div className="flex flex-col gap-4">
+        <h1 className="section-header">What&apos;s Included in Our Packages</h1>
+        <p>
+          All our packages include transportation, guided tours, entrance fees,
+          and bottled water. Certain packages include meals and additional
+          activities.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default IncludedInPackage;
